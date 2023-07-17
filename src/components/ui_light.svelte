b087e37fd23f30e396e48940bf59d034e4eae980
Rikamo1 <rik.amos.krajinovic@gmail.com>
2023-07-17 19:52:51 +0200

    Made the website with light and dark ui for light and dark backgrounds.

---
+++ src/components/ui_light.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  export let color = {}
+  
+
+  const dispatch = createEventDispatcher();
+  
+  function handleClick() {
+      dispatch("colorChange")
+  }
+ 
+  if (color.hex === undefined) {
+    color.hex = ""
+  }
+</script>
+
+<div id="UIContainer" class="grid h-screen items-center justify-center">
+      <div class="justify-center grid">
+        <h1 class="prose underline prose-2xl text-base-content">Colorpicker</h1>
+        <button class="btn btn-outline" on:click={handleClick}>New color</button>
+        <h2 class="prose prose-xl text-base-content">{color.hex.toUpperCase()}</h2>
+      </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+<script>
+  import UI_dark from "./../components/ui_dark.svelte"
+  import UI_light from "./../components/ui_light.svelte"
+  import {generateColor} from "./../javascript/generateColor.js"
+
+  let color = {}
+
+
+  function changeColor() {
+    color = generateColor()
+
   
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style> 
+    document.body.style.background = color.hex
+
+  }
+
+</script>
+
+{#if color.isLight}
+<UI_dark on:colorChange={changeColor} color={color} />
+
+{:else}
+<UI_light on:colorChange={changeColor} color={color} />
+
+{/if}
+
+
