543ace42e405f97d4ed1a52abb00dc91cb6bd2b2
Rikamo1 <rik.amos.krajinovic@gmail.com>
2023-07-18 10:44:44 +0200

    fixed style

--- src/javascript/generateColor.js
+++ src/javascript/generateColor.js
@@ -1,12 +1,10 @@
 
-    import tinycolor from "tinycolor2";
+import tinycolor from "tinycolor2"
     
-    function generateColor() {
-        let hex = "#" + Math.floor(Math.random()*16777215).toString(16);
-
-        let isLight = tinycolor(hex).isLight()
+function generateColor() {let hex = "#" + Math.floor(Math.random()*16777215).toString(16)
+    let isLight = tinycolor(hex).isLight()
         
-        return {hex, isLight}
-    }
+    return {hex, isLight}
+}
 
 export {generateColor}
